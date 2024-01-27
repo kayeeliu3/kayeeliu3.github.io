b747956b9983cdd332585dfe9472616ca36965cd
Kayee Liu <kasterune@gmail.com>
2024-01-27 21:01:38 +0000

    new background, toggle now consistent

--- basic.js
+++ basic.js
@@ -1,9 +1,26 @@
 var crt_toggle = document.getElementById("crttoggle");
 crt_toggle.addEventListener("click", function(e) {
-    var body_crttoggle = document.getElementById("bodycrt");
-    if (body_crttoggle.classList.contains('crt')) {
+    toggle()
+});
+
+function toggle() {
+    let body_crttoggle = document.getElementById("bodycrt");
+    if (localStorage.getItem("toggled") === "false") {
+        localStorage.setItem("toggled", "true");
+        body_crttoggle.classList.add("crt");
+    } else {
+        localStorage.setItem("toggled", "false");
+        body_crttoggle.classList.remove("crt");
+    }
+}
+
+function startup() {
+    let body_crttoggle = document.getElementById("bodycrt");
+    if (localStorage.getItem("toggled") === "false") {
         body_crttoggle.classList.remove("crt");
     } else {
         body_crttoggle.classList.add("crt");
     }
-});
+}
+
+startup();
